10b72d35758653899bdf46927baa52f9cd2f6772
saudkhanbpk <saudkhanbpk@gmail.com>
2023-04-28 14:20:29 +0500

    v2

---
+++ src/config/config.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+ 
+const firebaseConfig = {
+  apiKey: "AIzaSyCwOCBGoaaOAblZheXi9sa1EzQ-9Y-T3RY",
+  authDomain: "vulcan-web-1a194.firebaseapp.com",
+  projectId: "vulcan-web-1a194",
+  storageBucket: "vulcan-web-1a194.appspot.com",
+  messagingSenderId: "541964008236",
+  appId: "1:541964008236:web:e542881a6083f240c00a89"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
