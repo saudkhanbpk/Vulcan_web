97a66db19104142894e2c69e610603550ad99ffd
saudkhanbpk <saudkhanbpk@gmail.com>
2023-04-28 15:37:25 +0500

    footer added

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,11 +1,46 @@
-import React from 'react'
-import './Footer.scss'
+import React from "react";
+import "./Footer.scss";
 const Footer = () => {
   return (
     <div>
-      
+      <div className="Main_footer">
+        <div className="container-fluid bg-dark p-3">
+          <div className="row">
+            <div className="col-md-4">
+              <ul className="text-white">
+                <h3 className="text-white">Address</h3>
+                <li>Some Text</li>
+                <li>Some Text</li>
+                <li>Some Text</li>
+                <li>Some Text</li>
+                <li>Some Text</li>
+              </ul>
+            </div>
+            <div className="col-md-4">
+              <ul className="text-white">
+                <h3 className="text-white">Links</h3>
+                <li>Some Text</li>
+                <li>Some Text</li>
+                <li>Some Text</li>
+                <li>Some Text</li>
+                <li>Some Text</li>
+              </ul>
+            </div>
+            <div className="col-md-4">
+              <ul className="text-white">
+                <h3 className="text-white">Contact</h3>
+                <li>Some Text</li>
+                <li>Some Text</li>
+                <li>Some Text</li>
+                <li>Some Text</li>
+                <li>Some Text</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
