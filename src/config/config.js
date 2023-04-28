0f000925eab3f2d5b7b01bcb51efa224c5946510
saudkhanbpk <saudkhanbpk@gmail.com>
2023-04-28 14:55:21 +0500

    .env set

--- src/config/config.js
+++ src/config/config.js
@@ -6,11 +6,11 @@ import {getStorage} from "firebase/storage";
 const firebaseConfig = {
 
   apiKey: process.env.MY_API_KEY,
-  authDomain: "vulcan-web-1a194.firebaseapp.com",
-  projectId: "vulcan-web-1a194",
-  storageBucket: "vulcan-web-1a194.appspot.com",
-  messagingSenderId: "541964008236",
-  appId: "1:541964008236:web:e542881a6083f240c00a89"
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId:process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_MESSAGING_APP_ID,
 };
 
 // Initialize Firebase
