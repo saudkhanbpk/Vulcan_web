98f6c755153ef0f464ced9cef2f10937e40c4c48
saudkhanbpk <saudkhanbpk@gmail.com>
2023-05-01 12:39:01 +0500

    header fixed like figma

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,38 +4,15 @@ const Footer = () => {
   return (
     <div>
       <div className="Main_footer">
-        <div className="container-fluid bg-dark p-3">
+        <div className="container-fluid p-3">
           <div className="row">
-            <div className="col-md-4">
-              <ul className="text-white">
-                <h3 className="text-white">Address</h3>
-                <li>Some Text</li>
-                <li>Some Text</li>
-                <li>Some Text</li>
-                <li>Some Text</li>
-                <li>Some Text</li>
-              </ul>
+            <div className="col-md-6">
+
             </div>
-            <div className="col-md-4">
-              <ul className="text-white">
-                <h3 className="text-white">Links</h3>
-                <li>Some Text</li>
-                <li>Some Text</li>
-                <li>Some Text</li>
-                <li>Some Text</li>
-                <li>Some Text</li>
-              </ul>
+            <div className="col-md-6">
+              
             </div>
-            <div className="col-md-4">
-              <ul className="text-white">
-                <h3 className="text-white">Contact</h3>
-                <li>Some Text</li>
-                <li>Some Text</li>
-                <li>Some Text</li>
-                <li>Some Text</li>
-                <li>Some Text</li>
-              </ul>
-            </div>
+           
           </div>
         </div>
       </div>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,37 +4,34 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-
+import  logo  from "../Images/Logo.png";
+import Button from '@mui/material/Button';
 
 
 const Header = () => {
   return (
     <div>
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <Navbar collapseOnSelect expand="lg" className="Header_nav" >
       <Container>
-        <Navbar.Brand href="#home">VulcanLearning</Navbar.Brand>
+        <Navbar.Brand href="#home" className="d-flex align-items-center">
+          <img src={logo} className="img-fluid logoImage" alt="" />
+          <h4 className="VulLogo">Vulcan</h4>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link href="#features" className="nav_liddd">About</Nav.Link>
+            <Nav.Link href="#pricing" className="nav_liddd">How it Works</Nav.Link>
+            <Nav.Link href="#pricing" className="nav_liddd">Courses</Nav.Link>
+           
           </Nav>
           <Nav>
-            <Nav.Link href="#deets">More deets</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
-              Dank memes
-            </Nav.Link>
+            <Nav.Link href="#deets" className="nav_liddd">Become an Educator</Nav.Link>
+           
+           <div className="mt-1">
+            <Button variant="outlined" size="small" className="me-1" >Log In </Button>
+            <Button variant="contained" size="small">Sign Up</Button>
+            </div>
           </Nav>
         </Navbar.Collapse>
       </Container>
