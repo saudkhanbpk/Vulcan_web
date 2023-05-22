5cb243c0b180b7dea9c45cda975e165b98ad4f61
saudkhanbpk <saudkhanbpk@gmail.com>
2023-05-22 16:23:38 +0500

    scroll to top done

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { ThemeProvider } from '@mui/material';
-import theme from './Theme/theme.jsx';
+import theme from './Theme/themes.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
