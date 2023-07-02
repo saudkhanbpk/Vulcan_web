e3b7cf110c70ccddf9dcaf1bce1c6437010b1402
vulcanschool <vulcanschool@protonmail.com>
2023-07-02 03:48:57 -0700

    Update setupAccount.js

--- functions/setupAccount.js
+++ functions/setupAccount.js
@@ -7,25 +7,10 @@ exports.setupAccount = onCall((request) => {
   const uid = request.auth.uid
   const { firstName, lastName, email, password, number, isEducator } = request.data
 
-  admin.auth().createUser({
-    email,
-    password
-  })
-  .then(currentUser => {
-    db.logUser(currentUser.uid, `ACTION: Firebase Account Created: ` + `ID: ${currentUser.email}`)
+  db.logUser(currentUser.uid, `ACTION: Firebase Account Created: ` + `ID: ${currentUser.email}`)
 
-    db.createUser(uid, firstName, lastName, email, number, isEducator)
-    .catch(error => {
-      db.logUser(currentUser.uid, `ERROR: Database Account Created: ` + `ID: ${uid}: ${error}`)
-    });
-  })
+  db.createUser(uid, firstName, lastName, email, number, isEducator)
   .catch(error => {
-    db.logUnauth(`ERROR: Firebase Account Created: ` + `Email: ${email}: ${error}`)
-
-    return { isSuccess: false,  errorMessage: error };
-  })
-
-  return {
-    isSuccess: true
-  }
+    db.logUser(currentUser.uid, `ERROR: Database Account Created: ` + `ID: ${uid}: ${error}`)
+  });
 })
