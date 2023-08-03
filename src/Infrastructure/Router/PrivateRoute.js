a1593842f9105402b49f7895c0eff6841ab18f5a
saudkhanbpk <saudkhanbpk@gmail.com>
2023-08-03 12:24:19 +0500

    404 error page added, routes setup

--- src/Infrastructure/Router/PrivateRoute.js
+++ src/Infrastructure/Router/PrivateRoute.js
@@ -1,4 +1,4 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import {
   chooseModalEmailVerify,
@@ -11,17 +11,18 @@ export const PrivateOutlet = (props) => {
   const dispatch = useDispatch();
   const { children } = props;
   const { user, loading } = useAuthentication();
+  const navigate = useNavigate();
+
+  const style = {
+    display: "flex",
+    height: "100vh",
+    justifyContent: "center",
+    alignItems: "center",
+  };
 
   if (loading) {
     return (
-      <Box
-        sx={{
-          display: "flex",
-          height: "100vh",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
+      <Box sx={style}>
         <CircularProgress />
       </Box>
     );
@@ -29,9 +30,10 @@ export const PrivateOutlet = (props) => {
 
   if (!user) {
     dispatch(chooseModalLogin());
-    return <Navigate to="/" />;
-  } else if (!user?.emailVerified) {
+    return navigate("/");
+  } else if (!user.emailVerified) {
     dispatch(chooseModalEmailVerify());
+    return navigate("/");  
   } else {
     return (
       <>
