3710a961ee742ad636efd5e382945438a6dfac11
saudkhanbpk <saudkhanbpk@gmail.com>
2023-09-08 17:33:29 +0500

    function modified and working fine

--- functions/uploadImageAndSaveLink.js
+++ functions/uploadImageAndSaveLink.js
@@ -1,32 +1,20 @@
-const { onCall, HttpsError } = require("firebase-functions/v2/https") 
-const { Storage } = require("@google-cloud/storage") 
+const { getStorage } = require('firebase-admin/storage');
+const storage = getStorage();
 
-const storage = new Storage() 
-
-exports.uploadImage = onCall(async (data, context) => {
-  if (!context.auth) {
-    throw HttpsError(
-      "unauthenticated",
-      "You must be authenticated to upload an image."
-    ) 
-  }
-  const { base64Image } = data 
-  const bucket = storage.bucket()  // Use the default bucket
-  // Generating a unique filename
-  const fileName = `images/${Date.now()}_image.jpg` 
+exports.uploadImageAndSaveLink = async (request) => {
+  const { base64Image } = request
+  const bucket = storage.bucket()  
+  const fileName = `images/${Date.now()}_image.jpg`
   try {
-    const imageBuffer = Buffer.from(base64Image, "base64") 
-    // Uploading the image to Cloud Storage
+    const imageBuffer = Buffer.from(base64Image, "base64")
     await bucket.file(fileName).save(imageBuffer, {
       metadata: {
-        contentType: "image/jpeg",
+        contentType: "image/jpg",
       },
-    }) 
-    // Here is the URL of the uploaded image
-    const imageUrl = `https://storage.googleapis.com/${bucket.name}/${fileName}` 
-    return { imageUrl } 
+    })
+    const imageUrl = `https://storage.googleapis.com/${bucket.name}/${fileName}`
+    return { imageUrl }
   } catch (error) {
-    console.error("Error uploading image:", error) 
-    throw HttpsError("internal", "Error uploading the image.", error) 
+    console.error("Error uploading image:", error)
   }
-}) 
+}
