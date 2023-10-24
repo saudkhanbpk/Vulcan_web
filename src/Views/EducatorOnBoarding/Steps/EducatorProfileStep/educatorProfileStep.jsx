b4e9b973e83eaf5aed76a9a4f442e8940bb5dbfa
saudkhanbpk <saudkhanbpk@gmail.com>
2023-10-24 11:59:57 +0500

    comment deleted

--- src/Views/EducatorOnBoarding/Steps/EducatorProfileStep/educatorProfileStep.jsx
+++ src/Views/EducatorOnBoarding/Steps/EducatorProfileStep/educatorProfileStep.jsx
@@ -71,7 +71,6 @@ export const EducatorProfileStep = () => {
 
   // Define the isUrlValid function
   function isUrlValid(userInput) {
-    // const res = userInput.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
     const res = userInput.match(/(https?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_.~#?&//=]*)/g);
     return res !== null;
   }
