c299f815093251b64074fae960408541b0b8d9de
saudkhanbpk <saudkhanbpk@gmail.com>
2023-12-01 20:47:11 +0500

    done

--- functions/updateClassScheduleStep.js
+++ functions/updateClassScheduleStep.js
@@ -6,29 +6,28 @@ const dbCalls = require("./databaseCalls")
 
 exports.updateClassScheduleStep = onCall(async (request) => {
     const { firstClass, duration, times } = request.data
-    console.log(request.data)
-    // let isSuccess = true
-    // let errorMessage = null
-    // const uid = request.auth.uid
-    // const courseDetails = {}
+    let isSuccess = true
+    let errorMessage = null
+    const uid = request.auth.uid
+    const courseDetails = {}
 
-    // try {
-    //     if (firstClass) {
-    //         courseDetails.first_class = firstClass
-    //     }
-    //     if (duration) {
-    //         courseDetails.duration = duration
-    //     }
-    //     if (firstClass) {
-    //         courseDetails.times = times
-    //     }
+    try {
+        if (firstClass) {
+            courseDetails.first_class = firstClass
+        }
+        if (duration) {
+            courseDetails.duration = duration
+        }
+        if (firstClass) {
+            courseDetails.times = times
+        }
 
-    //     db.ref(`users/${uid}/educator/courses/pending/class_schedule`).update(courseDetails)
+        db.ref(`users/${uid}/educator/courses/pending/class_schedule`).update(courseDetails)
 
-    // } catch (error) {
-    //     dbCalls.logUser("ERROR: Class Schedule Step:" + error)
-    //     isSuccess = false
-    //     errorMessage = error
-    // }
+    } catch (error) {
+        dbCalls.logUser("ERROR: Class Schedule Step:" + error)
+        isSuccess = false
+        errorMessage = error
+    }
     return { isSuccess: isSuccess, errorMessage: errorMessage }
 })      
